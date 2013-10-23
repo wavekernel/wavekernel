6e14c48bac190c1156bb7c5d67f8db26805ef8b5
LennMars <lennmars@gmail.com>
2013-10-23 18:35:57 +0900

    add distributed residual norm checker for general problem

--- eigen_test/src/solver/solver_main.f90
+++ eigen_test/src/solver/solver_main.f90
@@ -58,48 +58,75 @@ contains
   end subroutine eigen_checker_local
 
 
-  subroutine eigen_checker_blacs_standard(arg, matrix_A, eigenpairs, &
-       res_norm_ave, res_norm_max)
+  subroutine eigen_checker_blacs(arg, matrix_A, eigenpairs, &
+       res_norm_ave, res_norm_max, matrix_B)
     include 'mpif.h'
 
     type(argument), intent(in) :: arg
     type(sparse_mat), intent(in) :: matrix_A
     type(eigenpairs_blacs), intent(inout) :: eigenpairs
+    type(sparse_mat), intent(in), optional :: matrix_B
     ! residual norm average, max
     double precision, intent(out) :: res_norm_ave, res_norm_max
 
     type(process) :: proc
-    integer :: dim, desc_Residual(desc_size), desc_A(desc_size)
-    double precision, allocatable :: Residual(:, :), matrix_A_dist(:, :)
+    integer :: dim, desc_Residual(desc_size), desc_A(desc_size), desc_B(desc_size)
+    double precision, allocatable :: Residual(:, :), matrix_A_dist(:, :), matrix_B_dist(:, :)
     ! ave_and_max is declared as array due to usage of bcast
     ! 3rd element is for the index of the max value (discarded currently)
     double precision :: norm, ave_and_max(3)
     integer :: j, owner_proc_col, ierr
     integer :: indxg2p ! ScaLAPACK function
 
+    if (arg%is_generalized_problem .and. .not. present(matrix_B)) then
+      stop '[Error] eigen_checker_blacs: matrix_B is not provided'
+    end if
+
+    call blacs_get(-1, 0, proc%context)
     call blacs_pinfo(proc%my_rank, proc%n_procs)
     call blacs_gridinfo(proc%context, proc%n_procs_row, proc%n_procs_col, &
          proc%my_proc_row, proc%my_proc_col)
 
     dim = arg%matrix_A_info%rows
+
     call setup_distributed_matrix(proc, dim, dim, desc_A, matrix_A_dist)
     call copy_global_sparse_matrix_to_local(matrix_A, desc_A, matrix_A_dist)
+
+    if (arg%is_generalized_problem) then
+      call setup_distributed_matrix(proc, dim, dim, desc_B, matrix_B_dist)
+      call copy_global_sparse_matrix_to_local(matrix_B, desc_B, matrix_B_dist)
+    end if
+
     call setup_distributed_matrix(proc, dim, arg%n_check_vec, desc_Residual, Residual)
 
-    ! Residual <- A * Eigenvectors
-    call pdsymm('L', 'L', dim, arg%n_check_vec, 1.0d0, &
-         matrix_A_dist, 1, 1, desc_A, &
-         eigenpairs%Vectors, 1, 1, eigenpairs%desc, &
-         0.0d0, Residual, 1, 1, desc_Residual)
+    if (arg%is_generalized_problem) then
+      ! Residual <- B * Eigenvectors
+      call pdsymm('L', 'L', dim, arg%n_check_vec, &
+           1.0d0, matrix_B_dist, 1, 1, desc_B, &
+           eigenpairs%Vectors, 1, 1, eigenpairs%desc, &
+           0.0d0, Residual, 1, 1, desc_Residual)
+    else
+      ! Residual <- Eigenvectors
+      call pdlacpy('A', dim, arg%n_check_vec, &
+           eigenpairs%Vectors, 1, 1, eigenpairs%desc, &
+           Residual, 1, 1, desc_Residual)
+    end if
 
-    ! For each column j, Residual(*, j) <- Residual(*, j) - eigenvalues(j) * Eigenvectors(*, j)
-    ! Then store the 2-norm of the residual column in the first row of the column
+    ! For each column j, Residual(*, j) <- Residual(*, j) * -eigenvalues(j)
     do j = 1, arg%n_check_vec
-      call pdaxpy(dim, -1.0d0 * eigenpairs%values(j), &
-           eigenpairs%Vectors, 1, j, eigenpairs%desc, 1, &
+      call pdscal(dim, -1.0d0 * eigenpairs%values(j), &
            Residual, 1, j, desc_Residual, 1)
-      call pdnrm2(dim, norm, Residual, 1, j, desc_Residual, 1)
+    end do
 
+    ! Residual <- Residual + A * Eigenvectors
+    call pdsymm('L', 'L', dim, arg%n_check_vec, &
+         1.0d0, matrix_A_dist, 1, 1, desc_A, &
+         eigenpairs%Vectors, 1, 1, eigenpairs%desc, &
+         1.0d0, Residual, 1, 1, desc_Residual)
+
+    ! Store 2-norm of each residual column in the first row of the column
+    do j = 1, arg%n_check_vec
+      call pdnrm2(dim, norm, Residual, 1, j, desc_Residual, 1)
       owner_proc_col = indxg2p(j, desc_Residual(block_col_), 0, &
            desc_Residual(csrc_), proc%n_procs_col)
       if (proc%my_proc_row == 0 .and. proc%my_proc_col == owner_proc_col) then
@@ -117,7 +144,7 @@ contains
 
     res_norm_ave = ave_and_max(1) / dble(arg%n_check_vec)
     res_norm_max = ave_and_max(2)
-  end subroutine eigen_checker_blacs_standard
+  end subroutine eigen_checker_blacs
 
 
   subroutine lib_eigen_checker(arg, matrix_A, eigenpairs, &
@@ -133,36 +160,17 @@ contains
 
     call check_master(arg%solver_type, is_master)
 
-    if (eigenpairs%type_number == 2 .and. .not. arg%is_generalized_problem) then
-      call eigen_checker_blacs_standard(arg, matrix_A, eigenpairs%blacs, &
-           res_norm_ave, res_norm_max)
-      return
-    end if
-
-    if (eigenpairs%type_number == 2) then ! Temporal implementation
-      eigenpairs%type_number = 1
-
-      allocate(eigenpairs%local%values(matrix_A%size))
-      eigenpairs%local%values(:) = eigenpairs%blacs%values(:)
-
-      if (is_master) then
-        allocate(eigenpairs%local%vectors(matrix_A%size, matrix_A%size))
-        eigenpairs%local%vectors(:, :) = 0.0d0
-      end if
-
-      call gather_matrix(eigenpairs%blacs%Vectors, eigenpairs%blacs%desc, &
-           0, 0, eigenpairs%local%vectors)
-
-      deallocate(eigenpairs%blacs%values, eigenpairs%blacs%Vectors)
-
-      if (.not. is_master) then
-        return
-      end if
-    end if
-
     if (eigenpairs%type_number == 1) then
       call eigen_checker_local(arg, matrix_A, eigenpairs, &
            res_norm_ave, res_norm_max, matrix_B)
+    else if (eigenpairs%type_number == 2) then
+      if (arg%is_generalized_problem) then
+        call eigen_checker_blacs(arg, matrix_A, eigenpairs%blacs, &
+             res_norm_ave, res_norm_max, matrix_B)
+      else
+        call eigen_checker_blacs(arg, matrix_A, eigenpairs%blacs, &
+             res_norm_ave, res_norm_max)
+      end if
     else
       print *, 'result checker for distributed output is not implemeted yet'
     end if
