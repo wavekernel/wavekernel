437bc3570ea9f1cfb4f3130f62628519e9b9e981
LennMars <lennmars@gmail.com>
2013-12-05 11:57:49 +0900

    refactor eigen_solver

--- eigen_test/src/solver/solver_main.f90
+++ eigen_test/src/solver/solver_main.f90
@@ -16,23 +16,17 @@ module solver_main
 contains
 
   subroutine eigen_solver(arg, matrix_A, eigenpairs, matrix_B)
-    use command_argument, only : argument
     use solver_lapack, only : eigen_solver_lapack
     use solver_scalapack_all, only : eigen_solver_scalapack_all
     use solver_scalapack_select, only : eigen_solver_scalapack_select
     use solver_eigenexa, only : setup_distributed_matrix_for_eigenexa, eigen_solver_eigenexa
-    use matrix_io, only : sparse_mat
-    use distribute_matrix, only : &
-         setup_distributed_matrix, distribute_global_dense_matrix, &
-         distribute_global_sparse_matrix
-    use eigenpairs_types, only : eigenpairs_types_union
 
     type(argument) :: arg
     type(sparse_mat), intent(in) :: matrix_A
     type(sparse_mat), intent(in), optional :: matrix_B
     type(eigenpairs_types_union), intent(out) :: eigenpairs
 
-    integer :: n, desc_A(9), desc_B(9)
+    integer :: n, desc_A(desc_size), desc_B(desc_size)
     type(process) :: proc
     double precision, allocatable :: matrix_A_dist(:, :), matrix_B_dist(:, :)
 
