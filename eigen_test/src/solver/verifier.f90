68628aa2f09c444f382ac1b44bda163f80cd5742
LennMars <lennmars@gmail.com>
2014-03-07 00:25:02 +0900

    fix process scope specification bug

--- eigen_test/src/solver/verifier.f90
+++ eigen_test/src/solver/verifier.f90
@@ -314,7 +314,7 @@ contains
     ! Aggregates in the direction of row
     call pdamax(n - 1, cos_max, cos_max_index1, Cos_maxes, 1, 1, desc_Cos_maxes, 1)
     cos_max = abs(cos_max)
-    call pielget('C', ' ', cos_max_index2, Cos_maxes_index, 1, cos_max_index1, desc_Cos_maxes_index)
+    call pielget('R', ' ', cos_max_index2, Cos_maxes_index, 1, cos_max_index1, desc_Cos_maxes_index)
     cos_max_index1 = cos_max_index1 + index1 - 1
     cos_max_index2 = cos_max_index2 + index1 - 1
     call pdasum(n - 1, cos_ave, Cos_sums, 1, 1, desc_Cos_sums, 1)
