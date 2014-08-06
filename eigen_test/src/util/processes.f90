f3fd485ee56069decf9623ab62a3f47bc90eaf83
LennMars <lennmars@gmail.com>
2014-08-06 19:37:02 +0900

    improve processor rayout decision

--- eigen_test/src/util/processes.f90
+++ eigen_test/src/util/processes.f90
@@ -57,24 +57,11 @@ contains
     integer, intent(in) :: n_procs
     integer, intent(out) :: n_procs_row, n_procs_col
 
-    integer :: n_procs_tmp, switch = 0, denom
-    n_procs_tmp = n_procs
-
-    n_procs_row = 1
-    n_procs_col = 1
-    do while (n_procs_tmp > 1)
-       denom = 2
-       do while (mod(n_procs_tmp, denom) /= 0)
-          denom = denom + 1
-       end do
-       if (switch == 0) then
-          n_procs_row = n_procs_row * denom
-       else
-          n_procs_col = n_procs_col * denom
-       end if
-       n_procs_tmp = n_procs_tmp / denom
-       switch = 1 - switch
+    n_procs_row = int(sqrt(dble(n_procs + 1)))
+    do while (mod(n_procs, n_procs_row) /= 0)
+      n_procs_row = n_procs_row - 1
     end do
+    n_procs_col = n_procs / n_procs_row
   end subroutine layout_procs
 
 
