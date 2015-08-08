00ff999701d9c00c58c74afd8753600516bf0a18
LennMar <lennmars@gmail.com>
2015-08-08 22:27:02 +0900

    version up

--- eigen_test/src/util/global_variables.f90
+++ eigen_test/src/util/global_variables.f90
@@ -3,6 +3,6 @@ module global_variables
 
   public :: g_block_size, g_version, g_mpi_wtime_init
   integer :: g_block_size = 128
-  character(*), parameter :: g_version = '20150625'
+  character(*), parameter :: g_version = '20150808'
   real(8) :: g_mpi_wtime_init = 0d0
 end module global_variables
