2844df201e12df0212979554006b471d9d4212ef
LennMars <lennmars@gmail.com>
2016-05-18 01:58:27 +0900

    add switcher for Makefile in ext-lib

--- elses/switch.sh
+++ elses/switch.sh
@@ -6,16 +6,19 @@ if [ "$1" = "true" ]; then
     cp -p ${ELSES_SRC}/code_for_mpi/elses-lib-mpi-wrapper.f90 ${ELSES_SRC}/elses-lib-mpi-wrapper-compile.f90
     cp -p ${ELSES_SRC}/code_optional/elses-qm-wavepacket-main-true.f90 ${ELSES_SRC}/elses-qm-wavepacket-main.f90
     cp -p ${ELSES_SRC}/code_optional/elses-la-eigen-solver-main-true.f90 ${ELSES_SRC}/elses-la-eigen-solver-main.f90
+    cp -p ext-lib/Makefile.true ext-lib/Makefile
 elif [ "$1" = "mpi" ]; then
     echo "switch to mpi"
     cp -p ${ELSES_SRC}/code_for_mpi/elses-lib-mpi-wrapper.f90 ${ELSES_SRC}/elses-lib-mpi-wrapper-compile.f90
     cp -p ${ELSES_SRC}/code_optional/elses-qm-wavepacket-main-dummy.f90 ${ELSES_SRC}/elses-qm-wavepacket-main.f90
     cp -p ${ELSES_SRC}/code_optional/elses-la-eigen-solver-main-dummy.f90 ${ELSES_SRC}/elses-la-eigen-solver-main.f90
+    cp -p ext-lib/Makefile.true ext-lib/Makefile
 elif [ "$1" = "dummy" ]; then
     echo "switch to dummy"
     cp -p ${ELSES_SRC}/code_for_mpi/elses-lib-mpi-wrapper-dummy.f90 ${ELSES_SRC}/elses-lib-mpi-wrapper-compile.f90
     cp -p ${ELSES_SRC}/code_optional/elses-qm-wavepacket-main-dummy.f90 ${ELSES_SRC}/elses-qm-wavepacket-main.f90
     cp -p ${ELSES_SRC}/code_optional/elses-la-eigen-solver-main-dummy.f90 ${ELSES_SRC}/elses-la-eigen-solver-main.f90
+    cp -p ext-lib/Makefile.dummy ext-lib/Makefile
 else
     echo "command: true / mpi / dummy"
 fi
