5a30dcdd59e528cca4bd138a884d428c30918cd7
LennMars <lennmars@gmail.com>
2016-08-19 17:56:44 +0900

    improve pratio plotter

--- elses/ext-lib/wavepacket/visualizer/plot_pratio.py
+++ elses/ext-lib/wavepacket/visualizer/plot_pratio.py
@@ -3,8 +3,11 @@ import argparse, json, pylab, sys, math, re
 
 kPsecPerAu = 2.418884326505e-5  # Time
 
-def plot(pratio_extract, time_start, time_end, title, fig_path):
+def plot(pratio_extract, printed_value, ymin, ymax, time_start, time_end, title, fig_path):
     pylab.rcParams['font.size'] = 16
+    fig = pylab.figure()
+    ax = fig.gca()
+    ax.ticklabel_format(useOffset=False)
 
     ts = map(lambda t: t * kPsecPerAu, pratio_extract['ts'])
 
@@ -20,8 +23,20 @@ def plot(pratio_extract, time_start, time_end, title, fig_path):
     pylab.grid(True)
 
     mark_size = 7
-    #pylab.plot(ts, pratio_extract['psi_pratios'], '+', label='psi', ms=mark_size)
-    pylab.plot(ts, pratio_extract['alpha_pratios'], '+', label='alpha', ms=mark_size)
+    if printed_value == 'alpha':
+        ys = pratio_extract['alpha_pratios']
+    elif printed_value == 'psi':
+        ys = pratio_extract['psi_pratios']
+    else:
+        assert(False)
+
+    if ymin is None:
+        ymin = 1.0
+    if ymax is None:
+        ymax = max(ys)
+    pylab.ylim(ymin, ymax)
+
+    pylab.plot(ts, ys, '+', label=printed_value, ms=mark_size)
 
     pylab.legend(loc='upper left')
     pylab.title(title)
@@ -31,11 +46,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('pratio_extract_path', metavar='JSON', type=str,
                         help='')
+    parser.add_argument('-p', metavar='VALUE', dest='printed_value', type=str, default='alpha',
+                        help='')
+    parser.add_argument('--ymin', metavar='Y_MIN', dest='ymin', type=float, default=None,
+                        help='')
+    parser.add_argument('--ymax', metavar='Y_MAX', dest='ymax', type=float, default=None,
+                        help='')
     parser.add_argument('-s', metavar='TIME_START', dest='time_start', type=float, default=None,
                         help='')
     parser.add_argument('-e', metavar='TIME_END', dest='time_end', type=float, default=None,
                         help='')
-    parser.add_argument('-t', metavar='TITLE', dest='title', type=str, default="",
+    parser.add_argument('-t', metavar='TITLE', dest='title', type=str, default='',
                         help='')
     parser.add_argument('-o', metavar='OUT', dest='fig_path', type=str, default=None,
                         help='')
@@ -53,4 +74,4 @@ if __name__ == '__main__':
 
     with open(args.pratio_extract_path, 'r') as fp:
         pratio_extract = json.load(fp)
-    plot(pratio_extract, args.time_start, args.time_end, title, fig_path)
+    plot(pratio_extract, args.printed_value, args.ymin, args.ymax, args.time_start, args.time_end, title, fig_path)
